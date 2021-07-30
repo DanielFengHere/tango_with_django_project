0390df1e70d6a39534274da6be53f4ddcd44b474
DanielFengHere <2537912f@student.gla.ac.uk>
2021-07-30 18:29:49 +0800

    modify forms

--- rango/forms.py
+++ rango/forms.py
@@ -21,6 +21,10 @@ class PageForm(forms.ModelForm):
                          help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
+    class Meta:
+        model = Page
+        exclude = ('category',)
+
     def clean(self):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
@@ -31,6 +35,3 @@ class PageForm(forms.ModelForm):
         return cleaned_data
 
 
-    class Meta:
-        model = Page
-        exclude = ('category',)
